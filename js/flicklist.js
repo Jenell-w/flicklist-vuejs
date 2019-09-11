ced7717212679d0d5710378290519d98fb7994a0
Jenell Weitz <jenellweitz@Jenells-MacBook-Pro.local>
2019-09-11 15:45:19 -0600

    work in progress studio2 almost

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,7 +1,7 @@
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "a334cd2c96ae880396654bbce6025ce6" // TODO 0 put your api key here
 }
 
 var flicklistView = new Vue({
@@ -12,28 +12,44 @@ var flicklistView = new Vue({
 			// Whenever it changes, Vue will automatically re-render
 			// the html for us.
 			watchlistItems: [],
-			browseItems: []
-		};
-	},
-	methods: {
-		discoverMovies: function () {
+	browseItems: [],
+	searchTerm: null,
+			};
+		},
+		methods: {
+			discoverMovies: function () {
 			/**
 			 * Makes an AJAX request to themoviedb.org, asking for some movies
 			 * if successful, updates the data.browseItems appropriately
 			 */
-
-			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
+				fetch(`${api.root}/discover/movie?api_key=${api.token}`)
 					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
-					.then((response) => {
-						console.log("We got a response from The Movie DB!");
+					.then((response => {
+						console.log("We got a response from the Movie DB");
 						console.log(response);
-						// TODO 2
-						// update this.browseItems, setting it equal to the movies we recieved in the response
 
-
+						this.browseItems = response.results;
 					});
+						// TODO 2- done
+						// update this.browseItems, setting it equal to the movies we recieved in the response
+                        //let this.browseItems = 
 		},
-		// TODO 5
+		searchMovies: function(searchTerm) {
+			console.log(`searching for movies with "${searchTerm}" in their title...`);
+
+			fetch(`${api.root}/search/movie?api_key=${api.token}&query=${searchTerm}`)
+			.then(resp = resp.ok ? resp.json() : Promise.reject(resp))
+			.then((response) => {
+				console.log("We heard from the Movie DB");
+				console.log(response);
+
+				this.browseItems = response.results;
+			});
+		},
+				addToWatchlist: function(movie) {
+					this.watchlistItems.push(movie);
+		        },
+		// TODO 5 -done
 		// make a method to use when a "Add to Watchlist" button is clicked
 		// It should accept a movie as a parameter, and add that item to
 		// the watchlistItems list,
